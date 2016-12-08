f3b1811a9138866e8ac2287d379b2cc257d97c0e
Matt Riemer <matthew@agilestyle.com>
2016-12-08 16:46:30 -0700

    permitted_params helper

--- lib/effective_roles.rb
+++ lib/effective_roles.rb
@@ -16,6 +16,10 @@ module EffectiveRoles
     yield self
   end
 
+  def self.permitted_params
+    {roles: []}
+  end
+
   def self.authorized?(controller, action, resource)
     if authorization_method.respond_to?(:call) || authorization_method.kind_of?(Symbol)
       raise Effective::AccessDenied.new() unless (controller || self).instance_exec(controller, action, resource, &authorization_method)
