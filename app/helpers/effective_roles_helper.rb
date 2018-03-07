9e50be7a2a2dc0668556293cc33cd760d2f99dc6
Matt Riemer <matthew@agilestyle.com>
2018-03-07 16:57:08 -0700

    drop support for formtastic

--- app/helpers/effective_roles_helper.rb
+++ app/helpers/effective_roles_helper.rb
@@ -1,23 +1,11 @@
 module EffectiveRolesHelper
-  # For use in formtastic forms
-  def effective_roles_fields(form, user = nil, options = {})
-    raise ArgumentError.new('EffectiveRoles config.role_descriptions must be a Hash.  The Array syntax is deprecated.') unless EffectiveRoles.role_descriptions.kind_of?(Hash)
-
-    roles = EffectiveRoles.assignable_roles_for(user, form.object)
-    descriptions = EffectiveRoles.role_descriptions[form.object.class.name] || EffectiveRoles.role_descriptions || {}
-
-    opts = {:f => form, :roles => roles, :descriptions => descriptions}.merge(options)
-
-    render :partial => 'effective/roles/fields', :locals => opts
-  end
-
   def effective_roles_summary(obj, options = {}) # User or a Post, any acts_as_roleable
     raise 'expected an acts_as_roleable object' unless obj.respond_to?(:roles)
 
     descriptions = EffectiveRoles.role_descriptions[obj.class.name] || EffectiveRoles.role_descriptions || {}
-    opts = {:obj => obj, :roles => obj.roles, :descriptions => descriptions}.merge(options)
+    opts = { obj: obj, roles: obj.roles, descriptions: descriptions }.merge(options)
 
-    render :partial => 'effective/roles/summary', :locals => opts
+    render partial: 'effective/roles/summary', locals: opts
   end
 
   # Output a table of permissions for each role based on current permissions
@@ -33,17 +21,17 @@ module EffectiveRolesHelper
 
     if opts[:only].present?
       klasses = Array(opts[:only])
-      render partial: '/effective/roles/summary_table', locals: {klasses: klasses, roles: roles}
+      render partial: '/effective/roles/summary_table', locals: { klasses: klasses, roles: roles }
       return
     end
 
     # Figure out all klasses (ActiveRecord objects)
     Rails.application.eager_load!
-    tables = ActiveRecord::Base.connection.tables - ['schema_migrations', 'delayed_jobs']
+    tables = ActiveRecord::Base.connection.tables - ['schema_migrations', 'delayed_jobs', 'active_storage_attachments']
 
-    klasses = ActiveRecord::Base.descendants.map do |model|
-      model if (model.respond_to?(:table_name) && tables.include?(model.table_name))
-    end.compact
+    klasses = ActiveRecord::Base.descendants.select do |model|
+      (model.respond_to?(:table_name) && tables.include?(model.table_name))
+    end
 
     if opts[:except]
       klasses = klasses - Array(opts[:except])
@@ -71,35 +59,35 @@ module EffectiveRolesHelper
       klasses = klasses + Array(opts[:additionally])
     end
 
-    render partial: '/effective/roles/summary_table', locals: {klasses: klasses, roles: roles}
+    render partial: '/effective/roles/summary_table', locals: { klasses: klasses, roles: roles }
   end
 
   def effective_roles_authorization_badge(level)
     case level
     when :manage
-      content_tag(:span, 'Full', class: 'label label-primary')
+      content_tag(:span, 'Full', class: 'label label-primary badge badge-primary')
     when :update
-      content_tag(:span, 'Edit', class: 'label label-success')
+      content_tag(:span, 'Edit', class: 'label label-success badge badge-success')
     when :update_own
-      content_tag(:span, 'Edit Own', class: 'label label-info')
+      content_tag(:span, 'Edit Own', class: 'label label-info badge badge-info')
     when :create
-      content_tag(:span, 'Create', class: 'label label-success')
+      content_tag(:span, 'Create', class: 'label label-success badge badge-success')
     when :show
-      content_tag(:span, 'Read only', class: 'label label-warning')
+      content_tag(:span, 'Read only', class: 'label label-warning badge badge-warning')
     when :index
-      content_tag(:span, 'Read only', class: 'label label-warning')
+      content_tag(:span, 'Read only', class: 'label label-warning badge badge-warning')
     when :destroy
-      content_tag(:span, 'Delete only', class: 'label label-warning')
+      content_tag(:span, 'Delete only', class: 'label label-warning badge badge-warning')
     when :none
-      content_tag(:span, 'No Access', class: 'label label-danger')
+      content_tag(:span, 'No Access', class: 'label label-danger badge badge-danger')
     when :yes
-      content_tag(:span, 'Yes', class: 'label label-primary')
+      content_tag(:span, 'Yes', class: 'label label-primary badge badge-primary')
     when :no
-      content_tag(:span, 'No', class: 'label label-danger')
+      content_tag(:span, 'No', class: 'label label-danger badge badge-danger')
     when :unknown
-      content_tag(:span, 'Unknown', class: 'label')
+      content_tag(:span, 'Unknown', class: 'label badge')
     else
-      content_tag(:span, level.to_s.titleize, class: 'label label-info')
+      content_tag(:span, level.to_s.titleize, class: 'label label-info badge badge-info')
     end
   end
 
@@ -108,13 +96,7 @@ module EffectiveRolesHelper
     return "#{klass.keys.first} #{klass.values.first}" if klass.kind_of?(Hash) && klass.length == 1
 
     klass = klass.keys.first if klass.kind_of?(Hash)
-    label = (klass.respond_to?(:name) ? klass.name : klass.to_s)
-
-    ['Effective::Datatables::'].each do |replace|
-      label = label.sub(replace, '')
-    end
-
-    label
+    klass.respond_to?(:name) ? klass.name : klass.to_s
   end
 
 end
